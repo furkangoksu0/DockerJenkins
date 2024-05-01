69432bee1caa498804e6a460924ef295fbd1c2d2
furkangoksu0 <furkangk20@gmail.com>
2024-05-01 14:11:30 +0300

    first

---
+++ src/main/java/com/example/dockerjenkinsdeneme/DockerJenkinsDenemeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkinsdeneme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsDenemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsDenemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dockerjenkinsdeneme/DockerJenkinsDenemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkinsdeneme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsDenemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
